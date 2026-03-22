1d2196562c9c965d521610d1da27e2ef9360a4eb
RAJ PRAKASHBHAI SOLANKI <100213146+rajsolanki125@users.noreply.github.com>
2026-03-22 20:22:56 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+async function loadMenu() {
+    const res = await fetch('../json/data.json');
+    const data = await res.json();
+
+    const menu = document.getElementById('menu');
+
+    data.sections.forEach((section, index) => {
+        const div = document.createElement('div');
+        div.classList.add('menu-section');
+
+        div.innerHTML = `
+            <div class="section-header" onclick="toggleSection(${index})">
+                ${section.name}
+                <span id="arrow-${index}">+</span>
+            </div>
+
+            <div class="section-body" id="section-${index}">
+                ${section.items.map(item => `
+                    <div class="menu-item">
+                        <div class="item-top">
+                            <div class="item-name">${item.name}</div>
+                            <div class="item-price">₹${item.price}</div>
+                        </div>
+                        <div class="item-desc">${item.description}</div>
+                    </div>
+                `).join('')}
+            </div>
+        `;
+
+        menu.appendChild(div);
+    });
+}
+
+function toggleSection(index) {
+    const section = document.getElementById(`section-${index}`);
+    const arrow = document.getElementById(`arrow-${index}`);
+
+    section.classList.toggle('active');
+    arrow.textContent = section.classList.contains('active') ? '-' : '+';
+}
+
+loadMenu();
